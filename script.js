385bdae9444609b88524656f093efc62a6849fb8
BlueVelocity <josephrodney@hotmail.com>
2023-08-21 00:54:16 -0400

    Update to etch-a-sketch visuals
    Added visual styling akin to etch-a-sketch
    Refactored javascript code for future feature implementation

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 let screen = document.getElementById('screen');
+let screenSize = 16;
 
-for (i = 0; i < 16; i++) {
+for (i = 0; i < screenSize; i++) {
     let pixelRow = document.createElement('div');
     pixelRow.setAttribute('id', `pixelRow${i}`);
     pixelRow.setAttribute('class', `pixelRow`);
     screen.appendChild(pixelRow);
 }
 
-let pixelRows = document.querySelectorAll('.pixelRow')
+let pixelRows = document.querySelectorAll('.pixelRow');
 let rowNumber = 0;
 
 //creates x pixels in each pixelRow
 pixelRows.forEach( (element) => {
-    for (i = 0; i < 16; i++) {
+    for (i = 0; i < screenSize; i++) {
         let pixel = document.createElement("div");
         pixel.setAttribute('id', `${rowNumber}-${i}`)
         pixel.setAttribute('class', 'pixel')
